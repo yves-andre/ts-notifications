18b34a8d971e2bb127cb22204d7c4a918c626d94
Emmanuel <emmanuel@empica.ch>
2022-11-14 13:52:13 +0100

    update

--- src/data/app-config.ts
+++ src/data/app-config.ts
@@ -1,9 +1,13 @@
 export const APP_CONFIG: any = {
-  APP_COLOR: "tileBlue",
-  APP_NAME: "Notifications",
+  APP_COLOR: 'tileBlue',
+  APP_NAME: 'Notifications',
   THEME_ASSETS: {
-    icons: "http://sp-resources-dev.dts.corp.local/tsf/assets/icons/",
-    placeholder: "http://sp-resources-dev.dts.corp.local/tsf/assets/images/placeholder/",
+    icons: 'http://sp-resources-dev.dts.corp.local/tsf/assets/icons/',
+    placeholder:
+      'http://sp-resources-dev.dts.corp.local/tsf/assets/images/placeholder/',
   },
-  THEME_FONTS: ["", ""],
-};
+  THEME_FONTS: [
+    'http://sp-resources-dev.dts.corp.local/tsf/assets/fonts/opensans.css',
+    'http://sp-resources-dev.dts.corp.local/tsf/assets/fonts/raleway.css',
+  ],
+}
--- src/pages/explorer/table/Table.tsx
+++ src/pages/explorer/table/Table.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Button,
   Picture,
@@ -7,36 +7,34 @@ import {
   Status,
   Table as TableUI,
   Tooltip,
-} from "@trading/energies-ui";
-import Notification from "../../../data/interfaces/notification";
-import { formatDate } from "../../../utils/formatters";
-import Highlight from "../../../components/Highlight/Highlight";
-import { useAppSelector } from "../../../hooks/use-app-selector";
-import { useAppDispatch } from "../../../hooks/use-app-dispatch";
-import { filtersActions } from "../../../store/filters-slice";
+} from '@trading/energies-ui'
+import Notification from '../../../data/interfaces/notification'
+import { formatDate } from '../../../utils/formatters'
+import Highlight from '../../../components/Highlight/Highlight'
+import { useAppSelector } from '../../../hooks/use-app-selector'
+import { useAppDispatch } from '../../../hooks/use-app-dispatch'
+import { filtersActions } from '../../../store/filters-slice'
 import {
   dismissNotification,
   setNotificationIsRead,
-} from "../../../store/notifications-slice";
+} from '../../../store/notifications-slice'
 
-import "./Table.scss";
-import { CATEGORY } from "../../../data/constants/category";
-import { STATUS } from "../../../data/constants/status";
+import './Table.scss'
+import { CATEGORY } from '../../../data/constants/category'
+import { STATUS } from '../../../data/constants/status'
 
 interface Props {
-  notifications: Notification[];
+  notifications: Notification[]
 }
 
 export const Table: React.FC<Props> = ({ notifications }) => {
-  const search = useAppSelector((state) => state.filters.searchFilter);
-  const sortFilter = useAppSelector((state) => state.filters.sortFilter);
-  const dispatch = useAppDispatch();
-  const selectedStatus = useAppSelector(
-    (state) => state.filters.selectedStatus
-  );
+  const search = useAppSelector((state) => state.filters.searchFilter)
+  const sortFilter = useAppSelector((state) => state.filters.sortFilter)
+  const dispatch = useAppDispatch()
+  const selectedStatus = useAppSelector((state) => state.filters.selectedStatus)
   const applications = useAppSelector(
     (state) => state.applications.applications
-  );
+  )
 
   const sortColumnHandler = (fieldName: string) => {
     // 1st click on column = order ascending,
@@ -45,57 +43,57 @@ export const Table: React.FC<Props> = ({ notifications }) => {
     if (sortFilter.field === fieldName) {
       sortFilter.asc
         ? dispatch(filtersActions.setSortFilter({ ...sortFilter, asc: false }))
-        : dispatch(filtersActions.setSortFilter({ field: "", asc: true }));
+        : dispatch(filtersActions.setSortFilter({ field: '', asc: true }))
     } else {
-      dispatch(filtersActions.setSortFilter({ field: fieldName, asc: true }));
+      dispatch(filtersActions.setSortFilter({ field: fieldName, asc: true }))
     }
-  };
+  }
 
   const openNotificationHandler = (notification: Notification) => {
-    window.open(notification.sourceUrl, "_blank", "noopener,noreferrer");
-    !notification.isRead && dispatch(setNotificationIsRead(notification._id));
-  };
+    window.open(notification.sourceUrl, '_blank', 'noopener,noreferrer')
+    !notification.isRead && dispatch(setNotificationIsRead(notification._id))
+  }
 
   const dismissNotificationHandler = (notification: Notification) => {
-    dispatch(dismissNotification(notification._id));
-  };
+    dispatch(dismissNotification(notification._id))
+  }
 
   const SortIcon: React.FC<{ field: string }> = ({ field }) => {
     if (sortFilter.field === field) {
       return sortFilter.asc ? (
         <Icon
-          name="caretRoundedDown"
-          size="small"
+          name='caretRoundedDown'
+          size='small'
           style={{ minWidth: 0, minHeight: 0, width: 15, height: 0 }}
         />
       ) : (
         <Icon
-          name="caretRoundedUp"
-          size="small"
+          name='caretRoundedUp'
+          size='small'
           style={{ minWidth: 0, minHeight: 0, width: 15, height: 0 }}
         />
-      );
+      )
     } else {
-      return <></>;
+      return <></>
     }
-  };
+  }
 
   const TD: React.FC<{
-    field: string;
-    children: string;
-    align?: "center" | "right" | "justify" | "char" | undefined;
+    field: string
+    children: string
+    align?: 'center' | 'right' | 'justify' | 'char' | undefined
   }> = ({ field, children, align }) => {
     return (
       <td
         onClick={() => sortColumnHandler(field)}
-        style={{ cursor: "pointer" }}
+        style={{ cursor: 'pointer' }}
         align={align}
       >
-        <Text variant="current">{children}</Text>&nbsp;
+        <Text variant='current'>{children}</Text>&nbsp;
         <SortIcon field={field} />
       </td>
-    );
-  };
+    )
+  }
 
   /* TODO : Service to get from Sharepoint */
   const getColorApplication = (title: string) => {
@@ -103,71 +101,71 @@ export const Table: React.FC<Props> = ({ notifications }) => {
       application.match
         .trim()
         .toLowerCase()
-        .split(",")
+        .split(',')
         .includes(title.trim().toLowerCase())
-    )?.txtColor;
-    return applicationColor || "red";
-  };
+    )?.txtColor
+    return applicationColor || 'red'
+  }
 
   const renderActionButtons = (notification: Notification) => {
     switch (notification.category) {
       case CATEGORY.ACTION_FEED:
         if (notification.isManual) {
           return (
-            <Tooltip title="mark as treated" placement="left">
+            <Tooltip title='Mark as Treated' placement='left'>
               <Button
-                size="small"
-                icon="close30-small"
+                size='small'
+                icon='close30-small'
                 iconOnly
-                color="#161719"
-                style={{ borderRadius: "50%" }}
+                color='#161719'
+                style={{ borderRadius: '50%' }}
                 onClick={() => dismissNotificationHandler(notification)}
               />
             </Tooltip>
-          );
+          )
         } else {
           return (
-            <Tooltip title="open to treat" placement="left">
+            <Tooltip title='Open to Treat' placement='left'>
               <Button
-                size="small"
-                icon="openAll"
+                size='small'
+                icon='openAll'
                 iconOnly
-                color="#161719"
-                style={{ borderRadius: "50%" }}
+                color='#161719'
+                style={{ borderRadius: '50%' }}
                 onClick={() => openNotificationHandler(notification)}
               />
             </Tooltip>
-          );
+          )
         }
       case CATEGORY.INFORMATION_FEED:
         return (
-          <Tooltip title="mark as treated" placement="left">
+          <Tooltip title='Dismiss' placement='left'>
             <Button
-              size="small"
-              icon="close30-small"
+              size='small'
+              icon='close30-small'
               iconOnly
-              color="#161719"
-              style={{ borderRadius: "50%" }}
+              color='#161719'
+              style={{ borderRadius: '50%' }}
               onClick={() => dismissNotificationHandler(notification)}
             />
           </Tooltip>
-        );
+        )
     }
-  };
+  }
 
   return (
-    <TableUI variant="feed">
+    <TableUI variant='feed'>
       <thead>
         <tr>
-          <TD field="title">Source</TD>
-          <TD field="subtitle">Subject</TD>
-          <TD field="description">Description</TD>
-          <TD field="details">Sent to</TD>
-          <TD field="date" align="right">
+          <TD field='title'>Source</TD>
+          <TD field='subtitle'>Subject</TD>
+          <TD field='description'>Description</TD>
+          <TD field='details'>Sent to</TD>
+          <TD field='date' align='right'>
             Date
           </TD>
           {selectedStatus !== STATUS.TREATED && (
-            <td align="right" width="100"></td>
+            <td align='right' width='100'></td>
           )}
         </tr>
       </thead>
@@ -180,7 +178,7 @@ export const Table: React.FC<Props> = ({ notifications }) => {
             <th>
               {!notification.isRead && (
                 <Status
-                  variant="badge"
+                  variant='badge'
                   color={getColorApplication(notification.title)}
                   style={{ marginLeft: -10, marginRight: 5 }}
                 ></Status>
@@ -190,28 +188,37 @@ export const Table: React.FC<Props> = ({ notifications }) => {
                   person
                   round
                   color={getColorApplication(notification.title)}
-                  size="small"
+                  size='small'
                   icon={notification.image}
                   style={{ marginRight: 10 }}
                 />
               )}
-              <Highlight highlight={search}>{notification.title}</Highlight>
+              <Text
+                color='rgba(255,255,255,.5)'
+                size='small'
+                uppercase
+                style={{ letterSpacing: '0.065em' }}
+              >
+                <Highlight highlight={search}>{notification.title}</Highlight>
+              </Text>
             </th>
             <th>
               <Highlight highlight={search}>{notification.subtitle}</Highlight>
             </th>
             <th>
-              <Highlight highlight={search}>
-                {notification.description}
-              </Highlight>
+              <Text light color='white'>
+                <Highlight highlight={search}>
+                  {notification.description}
+                </Highlight>
+              </Text>
             </th>
             <th>
-              <Text color="silver" italic light>
+              <Text color='rgba(255,255,255,.4)' italic light size='small'>
                 <Highlight highlight={search}>{notification.details}</Highlight>
               </Text>
             </th>
-            <th align="right">
-              <Text color="silver" light>
+            <th align='right'>
+              <Text color='rgba(255,255,255,.4)' italic light size='small'>
                 <Highlight highlight={search}>
                   {formatDate(notification.date)}
                 </Highlight>
@@ -219,7 +226,7 @@ export const Table: React.FC<Props> = ({ notifications }) => {
             </th>
             {selectedStatus !== STATUS.TREATED && (
               <th
-                align="right"
+                align='right'
                 style={{ paddingRight: 5 }}
                 onClick={(e) => e.stopPropagation()}
               >
@@ -230,7 +237,7 @@ export const Table: React.FC<Props> = ({ notifications }) => {
         ))}
       </tbody>
     </TableUI>
-  );
-};
+  )
+}
 
-export default Table;
+export default Table
