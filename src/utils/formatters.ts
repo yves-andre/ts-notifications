d2aac60e0b8576e5561caabe1aa1fdc8123b3e7f
Armin <armin@empica.ch>
2022-11-30 15:38:07 +0100

    Fix date format

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,14 +1,24 @@
 export const formatDate = (dateString: string) => {
   const date = new Date(dateString);
   const currentDate = new Date();
-  const time = `${date.getHours()}:${date.getMinutes()}`;
+  const time = date.toLocaleString('ch-FR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
 
   if (
     date.getDay() === currentDate.getDay() &&
     date.getMonth() === currentDate.getMonth() &&
     date.getFullYear() === currentDate.getFullYear()
   ) {
-    return time
+    return 'Today at ' + time;
+  } else if (
+    date.getDay() === currentDate.getDay() - 1 &&
+    date.getMonth() === currentDate.getMonth() &&
+    date.getFullYear() === currentDate.getFullYear()
+  ){
+    return 'Yesterday at ' + time;
   }
 
   const options:any = { year: 'numeric', month: 'short', day: 'numeric'};
