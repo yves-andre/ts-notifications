22054945b8b0ac027c0fd771bcb33841ae960a23
Armin <armin@empica.ch>
2023-01-19 13:51:47 +0100

    Kerberos update retrocompatibility

--- src/services/_http-client.ts
+++ src/services/_http-client.ts
@@ -1,13 +1,13 @@
 const getNewToken = async () => {
   const url = process.env.REACT_APP_API_DWP_AUTH as string;
-  const authResponse = await fetch(url, { 
-    credentials: "include" 
+  const authResponse = await fetch(url, {
+    credentials: "include"
   });
   const authData = await authResponse.json();
   return authData.token;
 };
 
-export const httpGet = async (
+export const httpGetAuth = async (
   route: string,
   config: object = {},
   authRequired: boolean = false
@@ -23,7 +23,7 @@ export const httpGet = async (
   // If authRequired is true, add the Authorization header
   if (authRequired) {
     let access_token = window.localStorage.getItem("token");
-    if(!access_token) {
+    if (!access_token) {
       access_token = await getNewToken();
       window.localStorage.setItem("token", access_token as string);
     }
@@ -48,7 +48,7 @@ export const httpGet = async (
   return await response.json();
 };
 
-export const httpPut = async (
+export const httpPutAuth = async (
   route: string,
   payload: object = {},
   config: object = {},
@@ -88,4 +88,49 @@ export const httpPut = async (
 
   if (!response.ok) throw new Error(`could not fetch data for url ${route}`);
   return await response.json();
+};
+
+
+const getVersion: any = async () => {
+  const versionResponse = await fetch("http://www.dpw-notifier.com/version");
+  const version = await versionResponse.text();
+  if (version === "old") {
+    return {
+      httpGet: async (route: string, config: object = {}) => {
+        if (!route) throw new Error("a route must be provided");
+        const response = await fetch(route, config);
+        if (!response.ok) throw new Error(`could not fetch data for url ${route}`);
+        return await response.json();
+      },
+      httpPut: async (route: string, payload: object = {}, config: object = {}) => {
+        if (!route) throw new Error("a route must be provided");
+        const PUTConfig = {
+          ...config,
+          method: "PUT",
+          headers: {
+            'Accept': "application/json, text/plain, */*",
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify(payload)
+        }
+        const response = await fetch(route, PUTConfig)
+        if (!response.ok) throw new Error(`could not fetch data for url ${route}`)
+        return await response.json()
+      }
+    };
+  } else {
+    return { httpGet: httpGetAuth, httpPut: httpPutAuth };
+  }
+}
+
+// Wrapper function that retrieves the correct httpGet function depending on the version
+export const httpGet = async (route: string, config: object = {}, authRequired: boolean = false) => {
+  const { httpGet } = await getVersion();
+  return await httpGet(route, config, authRequired);
+};
+
+// Wrapper function that retrieves the correct httpGet function depending on the version
+export const httpPut = async (route: string, payload: object = {}, config: object = {}, authRequired: boolean = false) => {
+  const { httpPut } = await getVersion();
+  return await httpPut(route, payload, config, authRequired);
 };
