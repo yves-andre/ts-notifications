f0b6af4de06b3402b0cb959434c25f40c053fcce
Armin <armin@empica.ch>
2023-01-24 10:24:19 +0100

    Use https

--- src/data/app-config.ts
+++ src/data/app-config.ts
@@ -2,15 +2,15 @@ export const APP_CONFIG: any = {
   APP_COLOR: 'tileBlue',
   APP_NAME: 'Notifications',
   THEME_ASSETS: {
-    icons: 'http://sp-resources-dev.dts.corp.local/tsf/assets/icons/',
+    icons: 'https://sp-resources-dev.dts.corp.local/tsf/assets/icons/',
     placeholder:
-      'http://sp-resources-dev.dts.corp.local/tsf/assets/images/placeholder/',
+      'https://sp-resources-dev.dts.corp.local/tsf/assets/images/placeholder/',
     totalLogo:
-      'http://sp-resources-dev.dts.corp.local/tsf/assets/images/logos/total-new.svg',
+      'https://sp-resources-dev.dts.corp.local/tsf/assets/images/logos/total-new.svg',
   },
   THEME_FONTS: [
-    'http://sp-resources-dev.dts.corp.local/tsf/assets/fonts/opensans/opensans.css',
-    'http://sp-resources-dev.dts.corp.local/tsf/assets/fonts/raleway/raleway.css',
+    'https://sp-resources-dev.dts.corp.local/tsf/assets/fonts/opensans/opensans.css',
+    'https://sp-resources-dev.dts.corp.local/tsf/assets/fonts/raleway/raleway.css',
   ],
   DEFAULT_APPLICATION_COLOR: '#0000006e',
 }
