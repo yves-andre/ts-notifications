0e593d285efa02e8ff4079b95383164100c4ce95
Armin <armin@empica.ch>
2023-03-20 15:10:07 +0100

    Fix username API call

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,4 +1,5 @@
 import { httpGet } from "./_http-client";
+const sessionStorageKey = 'notification_login_usernames';
 
 export const getUserLogin = async () => {
   const login = await httpGet(process.env.REACT_APP_API_AUTH_URL as string, {
@@ -16,6 +17,20 @@ export const getUserLogin = async () => {
 };
 
 export const getUserNameFromLogin = async (login: string) => {
+  let savedUsernames = {} as any;
+
+  try {
+    savedUsernames = sessionStorage.getItem(sessionStorageKey)
+      ? JSON.parse(sessionStorage.getItem(sessionStorageKey) as string)
+      : {};
+  } catch (error) {
+    console.error('Error parsing usernames from sessionStorage:', error);
+  }
+
+  if (savedUsernames[login]) {
+    return savedUsernames[login];
+  }
+
   const loginEncoded = encodeURIComponent(login).replace('%5C', '\\');
   const url = `${process.env.REACT_APP_API_URL}/Web/siteusers(@v)?@v='${loginEncoded}'`;
   const config = {
@@ -24,8 +39,31 @@ export const getUserNameFromLogin = async (login: string) => {
       Accept: 'application/json;odata=verbose'
     }
   }
-  const username =  await httpGet(url, config).then((response) => {
+  const username = await httpGet(url, config).then((response) => {
     return response?.d?.Title;
+  }).catch(error => {
+    console.error(`Error when fetching username from login : ${login}`, error);
+    saveUsername(login, login);
   })
+
+  if (username) {
+    saveUsername(login, username);
+  }
+
   return username;
+}
+
+const saveUsername = (login: string, username: string | null) => {
+  let savedUsernames = {} as any;
+
+  try {
+    savedUsernames = sessionStorage.getItem(sessionStorageKey)
+      ? JSON.parse(sessionStorage.getItem(sessionStorageKey) as string)
+      : {};
+  } catch (error) {
+    console.error('Error parsing usernames from sessionStorage:', error);
+  }
+  
+  savedUsernames[login] = username;
+  sessionStorage.setItem(sessionStorageKey, JSON.stringify(savedUsernames));
 }
