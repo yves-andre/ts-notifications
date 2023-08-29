a3809128fc7e7711ec4364c477ecaedf1d656a09
Emmanuel FERREIRA <emmanuel.ferreira@external.totsa.com>
2023-08-29 12:04:28 +0200

    fix css order wip

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    style: {
+      modules: {
+        localIdentName: '[local]_[hash:base64:4]',
+      },
+    },
+    webpack: {
+      plugins: {
+        remove: [ 'MiniCssExtractPlugin' ],
+        add:[
+          new MiniCssExtractPlugin({ 
+            filename: 'static/css/[name].[contenthash:8].css',
+            chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+            insert: '#root'
+          }),
+        ]
+      },
+    },
+  }
+  
