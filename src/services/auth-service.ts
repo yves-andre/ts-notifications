ddd331d1c192b70128a1bf2091ab02dab7e55704
Armin DERVISAGIC <armin.dervisagic@external.totsa.com>
2023-09-05 16:07:37 +0200

    auth-service update

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,6 +1,6 @@
 import { httpGet } from "./_http-client";
-const sessionStorageKey = 'notification_login_usernames';
 let usernamePromise: { [key: string]: Promise<string> | null } = {};
+const SESSION_STORAGE_KEY = 'notification_login_usernames';
 
 export const getUserLogin = async () => {
   const url = `${window.location.protocol}${process.env.REACT_APP_API_AUTH_URL}`;
@@ -31,73 +31,79 @@ export const getRawUserLogin = async () => {
   return login;
 };
 
-export const getUserNameFromLogin = async (login: string) => {
-  let savedUsernames = {} as any;
-
+/**
+ * Attempt to retrieve saved usernames from session storage.
+ */
+ const getSavedUsernames = (): Record<string, string> => {
   try {
-    savedUsernames = sessionStorage.getItem(sessionStorageKey)
-      ? JSON.parse(sessionStorage.getItem(sessionStorageKey) as string)
-      : {};
+    const savedData = sessionStorage.getItem(SESSION_STORAGE_KEY);
+    return savedData ? JSON.parse(savedData) : {};
   } catch (error) {
     console.error('Error parsing usernames from sessionStorage:', error);
+    return {};
   }
+};
 
+/**
+ * Save username in session storage.
+ */
+const saveUsername = (login: string, username: string): void => {
+  const usernames = getSavedUsernames();
+  usernames[login] = username;
+  sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(usernames));
+};
+
+/**
+ * Make HTTP request to get username from a given login.
+ */
+const fetchUsernameFromAPI = async (login: string): Promise<string> => {
+  const loginEncoded = encodeURIComponent(login).replace('%5C', '\\');
+  const url = `${window.location.protocol}${process.env.REACT_APP_API_URL}/Web/siteusers(@v)?@v='${loginEncoded}'`;
+
+  const config = {
+    contentType: "application/json",
+    headers: {
+      Accept: 'application/json;odata=verbose'
+    }
+  };
+
+  try {
+    const response = await httpGet(url, config);
+    if (typeof response === "number") {
+      throw new Error('Network response was not ok');
+    }
+    return response?.d?.Title || "";
+  } catch (error) {
+    console.error(`Error when fetching username from login: ${login}`, error);
+    return login || "";
+  }
+};
+
+export const getUserNameFromLogin = async (login: string): Promise<string> => {
+  const savedUsernames = getSavedUsernames();
+
+  // Check if we already have the username saved
   if (savedUsernames[login]) {
     return savedUsernames[login];
   }
 
+  // Check for ongoing username requests to avoid duplicate calls
   if (usernamePromise[login]) {
-    // If there's an ongoing username request for this login, wait for it
-    return await usernamePromise[login];
-  }
-
-  // Start a new username request for this login and store the promise
-  usernamePromise[login] = (async () => {
-    const loginEncoded = encodeURIComponent(login).replace('%5C', '\\');
-    const url = `${window.location.protocol}${process.env.REACT_APP_API_URL}/Web/siteusers(@v)?@v='${loginEncoded}'`;
-    const config = {
-      contentType: "application/json",
-      headers: {
-        Accept: 'application/json;odata=verbose'
-      }
-    };
-    
-    try {
-      const response = await httpGet(url, config);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const username = response?.d?.Title;
-      saveUsername(login, username); // Save the username immediately after retrieving it
-      return username;
-    } catch (error) {
-      console.error(`Error when fetching username from login : ${login}`, error);
-      saveUsername(login, login);
-      return login || "";
+    const result = await usernamePromise[login];
+    if (result) {
+      return result;
     }
-  })();
+  }
+
+  // Make the API request and cache the promise to avoid duplicate calls
+  usernamePromise[login] = fetchUsernameFromAPI(login);
+  // use nullish coalescing (returns login if await usernamePromise[login] is null or undefined)
+  const fetchedUsername = await usernamePromise[login] ?? login;
+  saveUsername(login, fetchedUsername);
 
   try {
-    // Wait for the username request to finish
-    const username = await usernamePromise[login];
-    return username;
+    return fetchedUsername;
   } finally {
-    // Clear the username promise for this login after the request is done
-    usernamePromise[login] = null;
+    usernamePromise[login] = null; // Actually clear the username promise
   }
-}
-
-const saveUsername = (login: string, username: string | null) => {
-  let savedUsernames = {} as any;
-
-  try {
-    savedUsernames = sessionStorage.getItem(sessionStorageKey)
-      ? JSON.parse(sessionStorage.getItem(sessionStorageKey) as string)
-      : {};
-  } catch (error) {
-    console.error('Error parsing usernames from sessionStorage:', error);
-  }
-
-  savedUsernames[login] = username;
-  sessionStorage.setItem(sessionStorageKey, JSON.stringify(savedUsernames));
-}
+};
