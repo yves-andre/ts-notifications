6a7c25ba50002b7518090194277cd126f562f912
Armin DERVISAGIC <armin.dervisagic@external.totsa.com>
2023-10-23 15:28:37 +0200

    rf test attachment files for demo

--- src/pages/test-validation/rf-test.tsx
+++ src/pages/test-validation/rf-test.tsx
@@ -62,40 +62,32 @@ const mock = {
             "type": "viewer",
             "files": [
               {
-                "title": "Word 1",
-                "url": "https://home-dev.dts.corp.local/dashboarddocuments/dashboard_libraries_0/lib_220912_636539b/TGP%20Files/Document.docx?d=wec6aa48d5d1a4dd7a2f9b0620f0e15c3",
+                "title": "",
+                "url": "https://home.dts.corp.local/dashboarddocuments/dashboard_libraries_10/lib_221019_f8a16e6/Fil<es-001fecc9/Fonctionnel%20-%20Specs%20v3.pdf?d=w1b44c46848194a65aff5727f77643d9f",
+                "iframe": "https://home.dts.corp.local/dashboarddocuments/_layouts/15/WopiFrame.aspx?sourcedoc=%7B1B44C468-4819-4A65-AFF5-727F77643D9F%7D&file=Fonctionnel%20-%20Specs%20v3.pdf&action=embedview",
+                "type": "office"
+              },
+              {
+                "title": "Image",
+                "url": "https://images.freeimages.com/images/large-previews/913/sea-3-1188161.jpg",
+                "type": "image"
+              },
+              {
+                "title": "Doc",
+                "url": "https://home.dts.corp.local/dashboarddocuments/dashboard_libraries_10/lib_221019_f8a16e6/Files-b99c4f1d/Release%20Note%20NOTIFIER.docx?d=w997674f7bb694ec9b3128627044cabbd",
                 "iframe": "https://home.dts.corp.local/dashboarddocuments/_layouts/15/WopiFrame.aspx?sourcedoc=%7B997674F7-BB69-4EC9-B312-8627044CABBD%7D&file=Release%20Note%20NOTIFIER.docx&action=embedview",
                 "type": "office"
               },
               {
-                "title": "File1",
-                "iframe": "https://home.dts.corp.local/dashboarddocuments/_layouts/15/WopiFrame.aspx?sourcedoc=%7BE41FB685-B22C-43BE-B1A8-E22AE5B53C9D%7D&file=Workbook.xlsx&action=embedview",
+                "title": "EML",
+                "url": "https://home.dts.corp.local/dashboarddocuments/dashboard_libraries_10/lib_221019_f8a16e6/Files-001fecc9/Old/Notifier V2.0 (Kerberos).eml",
                 "type": "office"
               },
               {
-                "title": "File2",
-                "url": "https://home-dev.dts.corp.local/dashboarddocuments/dashboard_libraries_0/dwp_db_lib_20200325_163945_2c333da/ms-viewer-16348197029/TS%20Forward_Dataviz_Juillet2021.pdf",
-                "type": "pdf"
-              },
-              {
-                "title": "Image 1",
-                "url": "https://images.unsplash.com/photo-1682687220198-88e9bdea9931?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2670",
-                "type": "image"
-              },
-              {
-                "title": "Image 2",
-                "url": "https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2575",
-                "type": "image"
-              },
-              {
-                "title": "Image 3",
-                "url": "https://images.unsplash.com/photo-1697325320142-28beaededbf3?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2664",
-                "type": "image"
-              },
-              {
-                "title": "Unsupported",
-                "url": "https://home.dts.corp.local/dashboarddocuments/dashboard_libraries_10/lib_221019_f8a16e6/Files-001fecc9/Old/Notifier",
-                "type": "eml"
+                "title": "XSLX",
+                "url": "https://home.dts.corp.local/dashboarddocuments/dashboard_libraries_10/lib_221019_f8a16e6/Files-001fecc9/Old/Workbook.xlsx?d=we41fb685b22c43beb1a8e22ae5b53c9d",
+                "iframe": "https://home.dts.corp.local/dashboarddocuments/_layouts/15/WopiFrame.aspx?sourcedoc=%7BE41FB685-B22C-43BE-B1A8-E22AE5B53C9D%7D&file=Workbook.xlsx&action=embedview",
+                "type": "office"
               },
             ]
           }
